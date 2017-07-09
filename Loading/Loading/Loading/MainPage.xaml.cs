b95813c6ec32100d0f0dfebcb2bc95ba8bba3838
Juliano <juliano.custodio@hotmail.com.br>
2017-07-09 16:50:42 -0300

    Implementing ActivityIndicator

--- Loading/Loading/Loading/MainPage.xaml.cs
+++ Loading/Loading/Loading/MainPage.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -12,6 +10,63 @@ namespace Loading
         public MainPage()
         {
             InitializeComponent();
+
+            IsLoading = false;
+            BindingContext = this;
+        }
+        public async void Show(object sender, EventArgs e)
+        {
+            try
+            {
+                aparece();
+
+                //Chame sua função aqui
+                await Task.Delay(4000);
+
+
+                some();
+            }
+            catch (Exception ex)
+            {
+                some();
+                if (ex != null)
+                {
+                    //Trate seu erro aqui
+                }
+            }
+        }
+        public async void aparece()
+        {
+            IsLoading = true;
+        }
+        public async void some()
+        {
+            IsLoading = false;
+        }
+
+        private bool isLoading;
+        public bool IsLoading
+        {
+            get
+            {
+                return this.isLoading;
+            }
+            set
+            {
+                this.isLoading = value;
+                RaisePropertyChanged("IsLoading");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void RaisePropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
         }
     }
 }
